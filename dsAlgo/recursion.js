34aa69cfb440c15608b3e5f55f620f04d6ebe6b7
nomadqwerty <davexmike72@gmail.com>
2022-07-07 13:57:50 +0100

    algoBaby

---
+++ dsAlgo/recursion.js
@@ -0,0 +1,38 @@
+// recursion is a function that calls it self, by breaking the problem down into smaller bits and then reassembling it back to whole
+
+// recursion property-
+//1- perform same operations multiple times with diff smaller inputs
+//2- we need a base condition. the base condition is a condition that will stop recursion if it is met
+
+// russian doll example
+
+const doll = (model)=>{
+
+    
+    // base
+    if(model==1) return 
+    // flow
+    doll(model-1)
+}
+doll(7)
+
+// why recursion? it is used to break down big problems
+// easier than iterative solutions (neither can replace neither)
+// used in many algorithms
+
+// how recursion works
+// method calls it self, reducing the value
+// exit loop
+// like so
+const recursion = (value)=> {
+    if(typeof value !== 'number')return 
+    // exit loop
+    if(value==0) return
+
+    // method self call, reducing the value
+    recursion(value-1)
+}
+
+// recursion in three steps
+// step 1
+// 
---
+++ dsAlgo/recursionProbs.js
@@ -0,0 +1,42 @@
+const assert = require('assert')
+// fibonaci seq
+const fib = (n)=>{
+    if(typeof n !== 'number'||n<0) return 'error'
+    if(n<=1){
+        return n
+    }else{
+        return fib(n-1)+fib(n-2)
+    }
+}
+console.log(fib(-1))
+// assume this funt is run with input of 4, n = 4
+// recursive walk tru
+// n = 4
+    // if n <=1 return
+    // 4 is > 1, skips condiotion
+
+    // recursive call of 4-1 and 4-2
+
+    // when n = 4-1
+    // if 3<=1, skips condition
+    // 1+1 is returned, 1+0 is returned.
+    // 1+1+1+0 = 3. fib of 4 = 3
+    // recursion on n-2
+        // if 4-2 <=1 , skips condition
+        // recursion for n-1
+        // if 2 - 1<=1, return n , n= 1
+        // if 2 -2<=1, return n, n = 0
+        // return 1+0
+    
+    // when n = 3-1
+    // if 2<=1, skips condition
+    // 1+0 is returned
+    //  recursion on n - 2
+        // if 3 -2<=1, return n- which is 1
+        // so 1+1 is returned to when n = 4
+    
+    // when n = 2-1
+    // if 1<=1, returns n which is 1
+    //  recursion on n -2
+        // if 2-2 <= 1, return 0
+        // so 1+0 is returned back to when n = 3
