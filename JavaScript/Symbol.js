c7f4b5fdc17f1beca3d7f33d13f654c89cda6e19
linux <davexmike72@gmail.com>
2022-08-11 21:06:55 +1300

    11.8

--- JavaScript/Symbol.js
+++ JavaScript/Symbol.js
@@ -1,5 +1,31 @@
-const unq = Symbol.for("keyGen")
+const unq = Symbol.for("keyGen");
 
-console.log(Symbol.keyFor(unq)==="keyGen")
+console.log(Symbol.keyFor(unq) === "keyGen");
 
-console.log(Object.getOwnPropertySymbols({}))
+console.log(Object.getOwnPropertySymbols({}));
+
+// symbol.hasInstance, well-known symbol, used to determine if a contructor object is an instance of another object.
+
+class Array1 {
+  static [Symbol.hasInstance](instance) {
+    return Array.isArray(instance);
+  }
+}
+
+console.log([] instanceof Array1);
+console.log({} instanceof Object);
+console.log("class", Array1[Symbol.hasInstance]([]));
+// .isConcatSpreadable
+const a = ["q", "w", "e"];
+const num = [1, 3, 4, 5];
+
+let aNum = a.concat(num);
+console.log(aNum);
+
+// make an array that cannot be concatenated to another array by setting .isConcatSpreadable option on Symbol to false
+
+num[Symbol.isConcatSpreadable] = false;
+
+aNum = a.concat(num);
+
+console.log(aNum);
