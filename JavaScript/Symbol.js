61f9075cfa10c2a79cf6b384adaeabe0e72e80cc
linux <davexmike72@gmail.com>
2022-08-20 05:54:00 +1300

    19.8

--- JavaScript/Symbol.js
+++ JavaScript/Symbol.js
@@ -29,3 +29,106 @@ num[Symbol.isConcatSpreadable] = false;
 aNum = a.concat(num);
 
 console.log(aNum);
+
+//////////////////////////////////////////////////
+//iterator and generators
+// iterators are objects that help return or expose properties one by one from another object,
+// generators are syntactical sugar to make using iterators easier
+
+// use * after function to build generators
+// use yield to yield a value to the generator from another object.
+const gen = function* () {
+  yield "a";
+};
+
+const iterate = gen();
+
+// the iter is a generator function whic has an inbuilt method called next(), the method triggers the yield to return what ever values was given to the generator.
+// in the generator fucn we can return values from an object that
+
+let obj = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+};
+let arr = [1, 2, 3, 4];
+const genny = function* () {
+  yield obj.a;
+  yield obj.b;
+  yield obj.c;
+  yield obj.d;
+  let i = 0;
+  yield arr[i];
+  i++;
+  yield arr[i];
+  i++;
+  yield arr[i];
+  i++;
+  yield arr[i];
+  i++;
+};
+let iter = genny();
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+
+let nonIter = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
+
+let count = -1;
+let iterObj = {
+  [Symbol.iterator]: function () {
+    return {
+      next: () => {
+        count++;
+        switch (count) {
+          case 0:
+            return {
+              value: nonIter.a,
+              done: false,
+            };
+          case 1:
+            return {
+              value: nonIter.b,
+              done: false,
+            };
+          case 2:
+            return {
+              value: nonIter.c,
+              done: false,
+            };
+          case 3:
+            return {
+              value: undefined,
+              done: true,
+            };
+          default:
+            return {
+              value: undefined,
+              done: true,
+            };
+        }
+      },
+    };
+  },
+};
+
+let data = iterObj[Symbol.iterator]();
+
+console.log(data.next());
+console.log(data.next());
+console.log(data.next());
+console.log(data.next());
+
+///////////////////////////////////////////////////////////
+// symbol.iterator
--- dsAlgo/sortAgain/elementary.js
+++ dsAlgo/sortAgain/elementary.js
@@ -1,20 +1,58 @@
-// bubble:
-// bubble sort aka sinking sort.
-// it repeatedly compares two side by side elements and swaps them if thy are in worng order.
-const arr = [37, 45, 29, 8, 12, 88, -3];
+// // bubble:
+// // bubble sort aka sinking sort.
+// // it repeatedly compares two side by side elements and swaps them if thy are in worng order.
+// const arr = [37, 45, 29, 8, 12, 88, -3];
 
-const bubbleSort = (array) => {
-  for (var i = arr.length; i > 0; i--) {
-    for (var j = 0; j < i - 1; j++) {
-      if (array[j] > array[j + 1]) {
-        // if ((array[j], array[j + 1] != undefined)) {
-        console.log(array[j], array[j + 1]);
-        [array[j], array[j + 1]] = [array[j + 1], array[j]];
-        // }
-      }
+// const bubbleSort = (array) => {
+//   for (var i = arr.length; i > 0; i--) {
+//     for (var j = 0; j < i - 1; j++) {
+//       if (array[j] > array[j + 1]) {
+//         // if ((array[j], array[j + 1] != undefined)) {
+//         console.log(array[j], array[j + 1]);
+//         [array[j], array[j + 1]] = [array[j + 1], array[j]];
+//         // }
+//       }
+//     }
+//   }
+//   return array;
+// };
+
+// console.log(bubbleSort(arr));
+
+//selectionSort
+const selected = (array) => {
+  // function to swap values
+  const swap = (array, i, j) => {
+    [array[i], array[j]] = [array[j], array[i]];
+  };
+
+  // 1st loop over array
+  for (var i = 0; i < array.length; i++) {
+    // create variable to store lowest number
+    var low = i;
+    // create second loop
+    for (var j = i + 1; j < array.length; j++) {
+      // if the low var is greater than j, update low with item j
+      if (array[low] > array[j]) low = j;
     }
+    // only swap if i is not equal to low var
+    if (i !== low) swap(array, i, low);
   }
   return array;
 };
 
-console.log(bubbleSort(arr));
+// console.log(selected([0, 2, 34, 22, 10, 19, 17]));
+
+const inserted = (array) => {
+  for (var i = 1; i < array.length; i++) {
+    var cur = array[i];
+    for (var j = i - 1; j >= 0 && array[j] > cur; j--) {
+      array[j + 1] = array[j];
+      console.log(array);
+    }
+    array[j + 1] = cur;
+  }
+  return array;
+};
+
+console.log(inserted([0, 2, 34, 22, 10, 19, 17]));
